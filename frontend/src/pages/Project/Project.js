13f90bf20d39b003f68c281bfd6ec7d4f8da2d0d
Robert Hartley <43013927+takuhartley@users.noreply.github.com>
2022-04-07 22:18:45 +0900

    Updated Project Display

--- frontend/src/pages/Project/Project.js
+++ frontend/src/pages/Project/Project.js
@@ -1,24 +1,28 @@
 import React from "react";
+import { useSpring, animated } from "react-spring";
 
 import { Link } from "@mui/material";
 export const Project = ({ project }) => {
+  const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } });
   return (
     <>
-      <div className="project_card">
-        <div className="project_card_title">
-          <Link href={`/project/${project._id}`}>{project.name}</Link>
+      <animated.div style={props}>
+        <div className="project_card">
+          <div className="project_card_title">
+            <Link href={`/project/${project._id}`}>{project.name}</Link>
+          </div>
+          <div className="project_card_title">
+            <div>{project.description}</div>
+          </div>
+          <div>
+            <img
+              className="project_card_header"
+              src={project.image}
+              alt={project.name}
+            />
+          </div>
         </div>
-        <div className="project_card_title">
-          <div>{project.description}</div>
-        </div>
-        <div>
-          <img
-            className="project_card_header"
-            src={project.image}
-            alt={project.name}
-          />
-        </div>
-      </div>
+      </animated.div>
     </>
   );
 };
